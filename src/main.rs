b024c740f25a3805673cb36da2a6af840034dca3
leduchien8901 <leduchien8901@gmail.com>
2023-07-09 15:49:15 +0700

    feat: source code ex2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
 fn main() {
-    println!("Hello, Rust Bootcamp by VBI Academy!");
+    exercise8();
+}
+
+fn exercise8() {
+    let mut accounting = vec!["Alice".to_string(), "Ben".to_string()];
+
+    loop {
+        let mut add_input = String::from("");
+        io::stdin()
+            .read_line(&mut add_input)
+            .expect("Failed to read line");
+
+        let add_vec: Vec<&str> = add_input.trim()[..].split_whitespace().collect();
+
+        if add_vec.len() < 1 {
+            println!("Incorrect input, try again");
+            continue;
+        }
+
+        let person = add_vec[0].to_string();
+        accounting.push(person);
+    }
 }
