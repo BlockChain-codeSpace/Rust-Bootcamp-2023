0bde750896972b74e4182e61c1245f066c9c1e57
leduchien8901 <leduchien8901@gmail.com>
2023-07-16 14:31:55 +0700

    fix: souce code exercises 4

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, Rust Bootcamp by VBI Academy!");
+    let numbers = vec![1, 2, 3, 4, 5];
+    // contains_value(&numbers, &3);
+    println!("Reversed strings: {:?}", contains_value(&numbers, &3));
+}
+
+fn contains_value<T: std::cmp::PartialEq>(collection: &[T], value: &T) -> bool {
+    if collection.is_empty() {
+        return false;
+    }
+    collection.contains(value)
 }
