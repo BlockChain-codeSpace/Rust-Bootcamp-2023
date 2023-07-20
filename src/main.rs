504ca768952c2a7ec11286826ab751f6169b4d3d
leduchien8901 <leduchien8901@gmail.com>
2023-07-20 14:45:41 +0700

    Submitting exercises 5 answers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::num::ParseIntError;
+
+
+fn parse_number(s: &str) -> Result<i32, ParseIntError> {
+    s.parse()
+}
+
+fn exercise2_should_work() {
+    assert_eq!(parse_number("42"), Ok(42));
+    assert_eq!(
+        parse_number("invalid").map_err(|e| e.to_string()),
+        Err("invalid digit found in string".parse().unwrap())
+    );
+}
+
 fn main() {
-    println!("Hello, Rust Bootcamp by VBI Academy!");
+    exercise2_should_work();
 }
